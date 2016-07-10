eb6a7fb51d3c315c0c6f6006361eb94798a44cdc
Hirofumi Inomata <digi-p@nifty.com>
2016-07-10 17:20:13 +0900

    Update gr_sketch.cpp

--- gr_sketch.cpp
+++ gr_sketch.cpp
@@ -1,4 +1,6 @@
-/*GR-KURUMI Sketch Template Version: V1.12*/
+/* GR-KURUMI Sketch Template Version: V1.12*/
+/* Modificated by DIGI-P (C) 2016 */
+/* This AS-IS file is offered with a BSD lite license */
 #include <Arduino.h>
 
 #include <stdlib.h>
